5010e69075429073e368c4d6acb25760af2e782f
Qing Wei <qing.wei@live.com>
2019-04-10 02:17:53 +0800

    Update bme680_i2c_example.py

--- testing/bme680_i2c_example.py
+++ testing/bme680_i2c_example.py
@@ -3,6 +3,12 @@ import board
 from busio import I2C
 import adafruit_bme680
 
+# To use 2nd i2c bus at GPIO pin 27(SDA_1)/28(SCL_1) on Jetson Nano:
+# - connect i2c sensor/oled i2c_sda  -->  jetson nano pin#27(sda_1)
+# - connect i2c sesnor/oled i2c_scl  -->  jetson nano pin#28(scl_1)
+# - connect i2c sensor/oled vcc(3v3) -->  jetson nano pin#17(3v3) or pin#2/4 if the sensor/oled board is 5v/3.3v compatible
+# - connect i2c sesnor/oled gnd      -->  jetson nano pin#14(gnd) or any available gnd pins on board
+
 # Create library object using our Bus I2C port
 i2c = busio.I2C(board.SCL_1, board.SDA_1)
 
